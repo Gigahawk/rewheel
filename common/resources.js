e6d1b4f155d7e43fa271b5237eab213839b443fc
CWP <77127867+Camwp@users.noreply.github.com>
2024-05-09 17:18:43 -0700

    ota fw

--- common/resources.js
+++ common/resources.js
@@ -5,7 +5,7 @@ export const resources = {
   "Shadowwheel(Local version)": {
     "sourceLink": "https://mega.nz/file/SRpSHAIC#PJ1S_X4yEgzXc4IIQi9DZvHO8WqyCj6uBu_OC6Y_p8I"
   },
-  "OTA Files Pint / GT & Firmware (IMPORTANT! REQUIRES IPFS COMPATIBLE BROWSER. RECOMMENDED: 'FIREFOX')": {
+  "OTA Files Pint / PintX(new!) / GT & Firmware (IMPORTANT! REQUIRES IPFS COMPATIBLE BROWSER. RECOMMENDED: 'FIREFOX')": {
     "sourceLink": "https://jade-competitive-dingo-303.mypinata.cloud/ipfs/QmVHdec2jDvNkMgpvRrm5uctKJGLd6sk9tcgg7YYaWAfYa"
   },
 }
