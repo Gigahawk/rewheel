d37767a8137a07d261f4d4d1e719badd0e35bb07
CWP <77127867+Camwp@users.noreply.github.com>
2024-05-09 17:19:05 -0700

    actual change for fw

--- common/resources.js
+++ common/resources.js
@@ -6,6 +6,6 @@ export const resources = {
     "sourceLink": "https://mega.nz/file/SRpSHAIC#PJ1S_X4yEgzXc4IIQi9DZvHO8WqyCj6uBu_OC6Y_p8I"
   },
   "OTA Files Pint / PintX(new!) / GT & Firmware (IMPORTANT! REQUIRES IPFS COMPATIBLE BROWSER. RECOMMENDED: 'FIREFOX')": {
-    "sourceLink": "https://jade-competitive-dingo-303.mypinata.cloud/ipfs/QmVHdec2jDvNkMgpvRrm5uctKJGLd6sk9tcgg7YYaWAfYa"
+    "sourceLink": "https://jade-competitive-dingo-303.mypinata.cloud/ipfs/QmQoL64nq6rLuf4MhxDfMzd332Zq5LhFn784knFdSFKg5H"
   },
 }
