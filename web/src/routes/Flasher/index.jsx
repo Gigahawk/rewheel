985fdbe81040748fae5ffaad5ea25a3712a37910
CWP <77127867+Camwp@users.noreply.github.com>
2024-06-11 17:14:37 -0700

    Revert "add button"
    This reverts commit 6463e4794602270f368a68f752db99808dfbe569.

--- web/src/routes/Flasher/index.jsx
+++ web/src/routes/Flasher/index.jsx
@@ -1,4 +1,4 @@
-import { Card, CardContent, CardHeader, Button, LinearProgress } from "@mui/material"
+import { Card, CardContent, CardHeader, Typography } from "@mui/material"
 import { Box } from "@mui/system"
 import { Flasher } from "./Flasher"
 import { useFirmwareLoader, useOnewheel } from "@hooks"
